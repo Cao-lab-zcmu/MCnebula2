618b6e0319f6ee1f2127f1d5af3b18eed3d86758
yellow <shaman.yellow@foxmail.com>
2022-11-15 09:43:36 +0800

    branch site

--- R/tools-yaml.R
+++
@@ -1,9 +0,0 @@
-# ==========================================================================
-# get or modify 'yaml' for 'report'
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-.yaml_default <- 
-  function(style = c("default", "BiocStyle", "BiocStyle_pdf")){
-    style <- match.arg(style)
-    readLines(system.file("extdata", paste0(style, ".yml"),
-                                 package = "MCnebula2"))
-  }
--- tests/testthat/test-compareSpectra.R
+++
@@ -1,3 +0,0 @@
-test_that("compareSpectra works", {
-            expect_equal(2 * 2, 4)
-})
