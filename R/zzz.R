79d6218708cea64ccc41cac7d01e92c16681937b
yellow <shaman.yellow@foxmail.com>
2023-03-31 13:56:09 +0800

    update from master

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,13 @@
 ## default font for visualization
-#' @importFrom grDevices pdfFonts
-.setFont <- function(pattern){
-  font <- names(pdfFonts())
-  n <- grep(pattern, font)
-  if (length(n) >= 1) {
-    font <- font[n[1]]
-  } else {
-    font[1]
-  }
-}
-.font <- .setFont("^Times")
+# @importFrom grDevices pdfFonts
+# .setFont <- function(pattern){
+  # font <- names(pdfFonts())
+  # n <- grep(pattern, font)
+  # if (length(n) >= 1) {
+  #   font <- font[n[1]]
+  # } else {
+  #   font[1]
+  # }
+# }
+
+.font <- if (.Platform$OS.type == 'unix') "Times" else "Times New Roman"
